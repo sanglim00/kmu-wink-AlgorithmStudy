f39ef6b5608d493c95d12953f4a7887dd84176e4
Sang-Lim <54923245+sanglim00@users.noreply.github.com>
2020-11-07 21:18:59 +0900

    Update 알고리즘 스터디 - 1일 될 때까지(그리디).cpp

--- 알고리즘 스터디 - 1일 될 때까지(그리디).cpp
+++ 알고리즘 스터디 - 1일 될 때까지(그리디).cpp
@@ -2,29 +2,23 @@
 
 using namespace std;
 
-int N, K, countN=0;
-
-void makeN() {
-	N = N - 1;
-	countN += 1;			
-	if (N % K != 0) makeN();	
-}
-
-void devideN() {	
-	if (N % K == 0) {
-		while (N != 1) {	
-			N = N / K;
-			countN += 1;
-		}
-	}
-	else {
-		makeN();
-		devideN();
-	}
+int checkN(int N, int K) {		// N 이 K의 배수인지 확인
+	if (N % K == 0) return 1;
+	else return 0;
 }
 
 int main() {
+	int N, K;
+	int countN = 0;
+
 	cin >> N >> K;
-	devideN();
+	
+	while (N != 1) {	// N이 1이 될 때까지 반복
+		if (checkN(N, K)) N = N / K;		// N이 K의 배수면 N은 N/K가 된다.
+		else {
+			N = N - 1;	// N이 K의 배수가 아니라면 N은 N-1이 된다.
+		}
+		countN += 1;		// 반복문 도는 동안 계산 횟수를 센다.
+	}
 	cout << countN << endl;
 }
