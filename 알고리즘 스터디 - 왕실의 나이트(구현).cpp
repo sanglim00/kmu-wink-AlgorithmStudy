412006d4ddabdf1cf0346232a11a2bcb7e1ecf55
Sang-Lim <54923245+sanglim00@users.noreply.github.com>
2020-11-11 01:29:11 +0900

    Update 알고리즘 스터디 - 왕실의 나이트(구현).cpp

--- 알고리즘 스터디 - 왕실의 나이트(구현).cpp
+++ 알고리즘 스터디 - 왕실의 나이트(구현).cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int checkROW(char row, int col) {
+int checkROW(char row, int col) {		//수평으로 2, 수직으로 1 이동 계산
 	int count = 0;
 	if ('b' < row && row < 'g') {
 		if (1 < col && col < 8) count += 4;
@@ -14,7 +14,7 @@ int checkROW(char row, int col) {
 	}
 	return count;
 }
-int checkCOL(char row, int col) {
+int checkCOL(char row, int col) {		//수직으로 2, 수평으로 1 이동 계산
 	int count = 0;
 	if ('a' < row && row < 'h') {
 		if (2 < col && col < 7)count += 4;
@@ -30,7 +30,6 @@ int checkCOL(char row, int col) {
 int main() {
 	char row;
 	int col;
-	int count = 0;
 
 	cin >> row >> col;
 	cout << checkROW(row, col) + checkCOL(row, col) << endl;
